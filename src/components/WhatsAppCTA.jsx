15999f13ea7d947485cd0e2f134b99baeb99ae00
Clementdevs <clementmwangi127@gmail.com>
2025-09-27 17:14:15 +0300

    Update WhatsAppCTA.jsx

--- src/components/WhatsAppCTA.jsx
+++ src/components/WhatsAppCTA.jsx
@@ -4,7 +4,7 @@ import { FaWhatsapp } from 'react-icons/fa'
 
 export default function WhatsAppCTA(){
   return (
-    <a href="https://wa.me/25472906509" target="_blank" rel="noreferrer" className="fab-cta">
+    <a href="https://wa.me/254729069509" target="_blank" rel="noreferrer" className="fab-cta">
       <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ type: 'spring', stiffness: 160 }} className="btn btn-success rounded-pill shadow-lg px-3 py-2 d-flex align-items-center">
         <FaWhatsapp className="me-2"/> Chat
       </motion.div>
